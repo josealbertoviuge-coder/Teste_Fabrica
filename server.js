afcc41b4de324c497a3fbc837392bb51f1d90046
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-05 14:21:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const pool = new Pool({
  host: "db.weqlfktnorahxteiypul.supabase.co",
  user: "postgres",
+ password: "Carbogas1234_GCQ",
  database: "postgres",
  port: 5432,
  ssl: { rejectUnauthorized: false }
 });
+
