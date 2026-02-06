f36970712fc96918dcfa423bd5ca3e07b25fbf81
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-05 21:58:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,23 +21,36 @@ app.get("/", (req, res) => {
   res.send("API rodando");
 });
 
-const result = await pool.query(`
-SELECT 
-  p.codigo_peca,
-  p.descricao,
-  e.nome_etapa,
-  a.status,
-  a.data
-FROM pecas p
-JOIN andamento_pecas a ON p.id_peca = a.id_peca
-JOIN etapas e ON a.id_etapa = e.id_etapa
-WHERE p.codigo_peca = $1
-ORDER BY a.data DESC
-`, [codigo]);
+app.get("/peca/:codigo", async (req, res) => {
+  try {
+    const codigo = req.params.codigo;   // 👈 ESTA LINHA
+
+    const result = await pool.query(`
+      SELECT 
+        p.codigo_peca,
+        p.descricao,
+        e.nome_etapa,
+        a.status,
+        a.data
+      FROM pecas p
+      JOIN andamento_pecas a ON p.id_peca = a.id_peca
+      JOIN etapas e ON a.id_etapa = e.id_etapa
+      WHERE p.codigo_peca = $1
+      ORDER BY a.data DESC
+    `, [codigo]);
+
+    res.json(result.rows);
+
+  } catch (err) {
+    console.error("ERRO QUERY:", err);
+    res.status(500).send(err.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Servidor iniciado");
 });
+
 
 
 
