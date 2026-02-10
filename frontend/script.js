4c97ce1111905341080a10a15ac086254b09d90c
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-10 15:07:55 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -239,7 +239,7 @@ function montarGraficoGantt(dados) {
   // TAMANHO HORIZONTAL (SCROLL)
   // =======================
   const larguraPorDia = 220; // px
-  const diasVisiveis = 14;
+  const diasVisiveis = 7;
 
   const diasTotais =
     (maxData - minData) / (1000 * 60 * 60 * 24);
@@ -579,6 +579,7 @@ function mostrarTempoTotal(horas) {
       ? `⏱ Tempo total da peça: ${dias}d ${resto}h`
       : `⏱ Tempo total da peça: ${horas.toFixed(1)}h`;
 }
+
 
 
 
