43701de60304092a4eae0d1bf0ef08c835f59f0e
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-17 19:05:33 -0300

    Update relatorio.js

--- frontend/relatorio.js
+++ frontend/relatorio.js
@@ -156,9 +156,6 @@ async function carregarRelatorio() {
   const codigo = params.get("codigo");
   const tagSelecionada = params.get("tag");
 
-  document.getElementById("dataISO").innerText =
-  new Date().toLocaleDateString("pt-BR");
-
   if (!codigo) {
     alert("Código da OP não informado.");
     return;
