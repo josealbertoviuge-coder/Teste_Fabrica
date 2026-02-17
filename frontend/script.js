38dcbf2b8ee59170eb72097671f13e0a15456cd8
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-16 23:59:00 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,7 @@ function abrirRelatorio(){
   }
 
   const url =
-    "/pdf/" +
+    "https://teste-fabrica.onrender.com/pdf/" +
     encodeURIComponent(codigo) +
     "?tag=" +
     encodeURIComponent(tagAtual);
@@ -863,6 +863,7 @@ setInterval(() => {
   if (chartGantt) chartGantt.update();
   if (chartDuracao) chartDuracao.update();
 }, 300);
+
 
 
 
