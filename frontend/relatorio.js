3e9a6887ddc28c1a43cb4a94bdc4c2478aae2a7b
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-17 19:12:47 -0300

    Update relatorio.js

--- frontend/relatorio.js
+++ frontend/relatorio.js
@@ -282,12 +282,10 @@ bloco.innerHTML = `
   <table id="tabela_${i}"></table>
   <canvas id="gantt_${i}"></canvas>
   <canvas id="duracao_${i}"></canvas>
-
-/*
   <div class="footer-page">
     Carbogas Ltda. • MTS v1.0 • Manufacturing Tracking System
   </div>
-*/
+
 `;
 
     container.appendChild(bloco);
